e3e4bf7cf7adfc5ebbf1f8a89b4a8ee068ca94f2
Corentin JACQUES <cjacques@Corentins-Mac.local>
2016-05-21 15:31:19 +0200

    opti makefile

--- ASM/zork.s
+++ ASM/zork.s
@@ -5,15 +5,13 @@
   
 ;mdr l1:
 l2:
-l2:		sti r1, %3609172917091723091730970789234678,r1 ;haha 
-		st r1, 123456
-		st r16, 1
+l2:		sti r1       , %3609970789234678,r1 ;haha 
+		st r1,						123456
+		st r16		,   1
 
 
-
-l1:      
+														l1:      
 						#Buff
+live:	live	     %:live
 
-live:	live %:live
-
-zjmp								    %:live #bonjour
+     zjmp									    %:live #bonjour
